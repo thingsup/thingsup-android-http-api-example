183336dcfbf2d5d4ad55854650914e370fa3d9b9
Pranav Naiknavare <pranavnaiknavare@Pranavs-MacBook-Pro.local>
2020-09-21 16:22:22 +0530

    Initial Commit

---
+++ app/src/main/java/io/thingsup/httpexample/MainActivity.java
@@ -0,0 +1,75 @@
+package io.thingsup.httpexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mTextView;
+    Button mSendButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.textview);
+        mSendButton = findViewById(R.id.sendButton);
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                request();
+            }
+        });
+    }
+
+
+    void request()
+    {
+        String url = "https://api.thingsup.io/data/3p309x1kcaks5he/aj4v59";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        mTextView.setText(mTextView.getText().toString()+"\n$:>"+"Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        mTextView.setText(mTextView.getText().toString()+"\n$:>"+"Error: " + error.toString());
+                    }
+                })
+            {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Authorization", "Bearer test");
+
+                return params;
+            }
+        };
+
+        MainApplication.getInstance().requestQueue.add(jsonObjectRequest);
+    }
+}
---
+++ app/src/main/java/io/thingsup/httpexample/MainApplication.java
@@ -0,0 +1,44 @@
+package io.thingsup.httpexample;
+
+import android.app.Application;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+
+public class MainApplication extends Application {
+
+
+    private static MainApplication application;
+    public RequestQueue requestQueue;
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        application = this;
+
+        // Instantiate the cache
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the RequestQueue with the cache and network.
+        requestQueue = new RequestQueue(cache, network);
+
+        // Start the queue
+        requestQueue.start();
+
+    }
+
+    public static MainApplication getInstance()
+    {
+        return application;
+    }
+
+
+}
